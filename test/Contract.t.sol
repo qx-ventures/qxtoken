70c21fc894f4388547e1507ce3dd2ceceb0ec6e3
Teodor Bogoeski <teodor@qx.ventures>
2023-05-19 16:28:40 +0200

    initial commit

---
+++ test/Contract.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Contract.sol";
+
+contract ContractTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
